b47cb9683e4890a373da6e00cbdb771063128e7d
Valurk <73291263+Valurk@users.noreply.github.com>
2022-03-14 01:50:11 -0300

    Add files via upload

---
+++ WiProtect.py
@@ -0,0 +1,40 @@
+import pyshark
+import os
+import sys
+from termcolor import colored
+
+
+if not os.geteuid()==0:
+    sys.exit('This script must be run as root!')
+
+
+os.system('sudo airmon-ng check kill') # kill any processes that might interfere with this script
+os.system('clear')
+
+os.system('sudo airmon-ng start wlan0') # putting the interface in the monitor mode for capture the packets
+os.system('clear')
+
+class style():
+    
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    
+
+print(style.GREEN + '''
+ __          ___ _____           _            _   
+ \ \        / (_)  __ \         | |          | |  
+  \ \  /\  / / _| |__) | __ ___ | |_ ___  ___| |_ 
+   \ \/  \/ / | |  ___/ '__/ _ \| __/ _ \/ __| __|
+    \  /\  /  | | |   | | | (_) | ||  __/ (__| |_ 
+     \/  \/   |_|_|   |_|  \___/ \__\___|\___|\__|
+                                                                                                          
+''')
+print(style.GREEN + "Listening..." , end = '\n\n')
+
+
+captureAndFilter = pyshark.LiveCapture(interface='wlan0mon',display_filter = 'wlan.fixed.reason_code != 0x0003') #reason code correctly is 0x0003
+#for packet in capture.sniff_continuously():
+
+for packet in captureAndFilter:
+    print(captureAndFilter, end='\r') #reformatting the original output is: <LiveCapture (0 packets)>
+    print(style().RED + "DEAUTH ATTACK DETECTED!  ") 
